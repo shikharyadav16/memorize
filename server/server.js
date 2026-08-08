392e17182061bb386a07cd00fdd671e6323a3b40
Shikhar Yadav <yadavshikhar49@gmail.com>
2026-08-09 02:08:15 +0530

    few changes

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "https://memoriize.vercel.app/"],
+  origin: ["http://localhost:5173", "https://memoriize.vercel.app"],
   credentials: true
 }));
 app.use(express.json());
